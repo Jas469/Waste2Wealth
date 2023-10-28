c86de89e1f89c36fb2b07c0f838ca53ebbccf4bf
Jashanjot <kaurjashanjot16@gmail.com>
2023-10-28 19:21:14 +0530

    fgfkg

---
+++ pages/VendorBuy.js
@@ -0,0 +1,135 @@
+  {/* import Header from './Header';
+ import {GoOrganization} from 'react-icons/go'; */}
+
+export default function VendorBuy(){
+    return(<>
+
+
+    {/* <Header/> */}
+    <div className= " mt-2 p-2 flex flex-row w-full h-auto overflow-y-hidden ">
+
+    <div className=" py-3 px-3 flex-col h-[100vh] w-full border-l-2 border-black scrollbar-thin scrollbar-track-slate-200 scrollbar-thumb-slate-400 bg-slate-800 text-black  overflow-y-scroll overflow-x-auto xsm:hidden">
+
+    {/* <div className=" w-full mt-2  h-8 items-center">
+    <div className=" text-3xl text-center font-bold text-white">Select Organisation</div>
+</div> */}
+
+
+
+<div className=" w-full mt-8 h-8 items-center">
+    <div className=" text-3xl text-center font-bold text-white">Filter Locations</div>
+</div>
+
+<div className=' mt-8 '>
+<div className='flex-col space-y-2'>
+
+ <div className=' text-white flex-col w-full space-y-3'> 
+  <div className="flex justify-center " ><div><button className="h-10 w-36 text-center text-white rounded-full px-3 pt-3 pb-10 hover:bg-violet-200 hover:text-black bg-fixed text-xl font-medium ">Mohali</button> </div></div>
+  <div className="flex justify-center " ><div><button className="h-10 w-36 text-center text-white rounded-full px-3 pt-3 pb-10 hover:bg-violet-200 hover:text-black text-xl font-medium ">Kharar</button> </div></div>
+  <div className="flex justify-center " ><div><button className="h-10 w-36 text-center text-white rounded-full px-3 pt-3 pb-10 hover:bg-violet-200 hover:text-black text-xl font-medium ">Chandigarh</button> </div></div>
+  <div className="flex justify-center " ><div><button className="h-10 w-36 text-center text-white rounded-full px-3 pt-3 pb-10 hover:bg-violet-200 hover:text-black text-xl font-medium ">Panchkula</button> </div></div>
+  <div className="flex justify-center " ><div><button className="h-10 w-36 text-center text-white rounded-full px-3 pt-3 pb-10 hover:bg-violet-200 hover:text-black text-xl font-medium ">Zirakpur</button> </div></div>
+  <div className="flex justify-center " ><div><button className="h-10 w-36 text-center text-white rounded-full px-3 pt-3 pb-10 hover:bg-violet-200 hover:text-black text-xl font-medium ">Kurali</button> </div></div>
+  <div className="flex justify-center " ><div><button className="h-10 w-36 text-center text-white rounded-full px-3 pt-3 pb-10 hover:bg-violet-200 hover:text-black text-xl font-medium ">Ropar</button> </div></div> 
+  <div className="flex justify-center " ><div><button className="h-10 w-36 text-center text-white rounded-full px-3 pt-3 pb-10 hover:bg-violet-200 hover:text-black text-xl font-medium ">Dera Bassi</button> </div></div> 
+
+
+    </div>
+    </div>
+
+   </div>
+
+</div>
+
+<div className="  w-[90%]  bg-zinc-100 border-b-4 ">
+<div className=" pt-12 text-center text-4xl font-serif font-semibold">Buy Now </div>
+
+{/* <div className=" mt-10 px-4 flex flex-row justify-center space-x-10"> */}
+    {/* <div className="flex-col h-[340px] w-[300px] rounded-lg mb-5 shadow-2xl hover:scale-95 transition duration-300 ease-in ">
+
+    <div className=" flex justify-center"><img className=" w-55 h-60 mt-2" src="work.png" alt="" /></div>
+    <div className="px-1 mt-1 text-xs text-left font-bold">Category </div>
+
+                    <div className="px-1 text-sm">We are an organisation which is working for the well-being of the society and its people.</div>
+                    <button className='bg-green-600 w-[90%] mt-4 rounded-md p-1 flex justify-center ml-2 text-white'>
+                        View 
+                    </button>
+    </div> */}
+    {/* <div className="flex-col h-[400px] w-[350px] rounded-lg mb-5 shadow-2xl hover:scale-95 transition duration-300 ease-in bg-slate-400 ">
+                    <div className=" flex justify-center"><img className=" w-52 h-48 mt-2" src="work1.jpg" alt="" /></div>
+                    <div className="px-1 mt-4 text-lg font-bold">NGO NAME</div>
+                    <div className="px-1 text-sm">We are an organisation which is working for the well-being of the society and its people.</div>
+                    <button className='bg-cyan-100 w-[90%] mt-5 rounded-md p-1'>
+                        View 
+                    </button>
+    </div> */}
+
+
+
+
+
+        <div className='w-[100%] '>
+            <div className='flex justify-evenly'>
+            <div className="flex-col h-[400px] border-2 w-[300px] shadow-xl rounded-lg mb-5 hover:scale-95 transition duration-100 ease-in xsm:h-auto bg-slate-400">
+                <div className=""><img className="rounded-t w-[350px] m-auto h-48 xsm:h-44" src="img1.png" alt="" /></div>
+                <div className="flex justify-between">
+                    <div className="bgimg bg-no-repeat text-black px-1 mt-1 text-xs py-1 rounded-tr rounded-br">Event Name</div>
+                    <div className="bgimg  mt-1 text-xs px-1 py-1 rounded-tl rounded-bl">Date</div>
+                </div>
+                <div className="px-1 mt-1 font-bold text-center xsm:text-sm">Title</div>
+                <div className="px-2 xsm:text-xs mt-1 text-justify text-sm ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora distinctio sit cum fuga consequuntur ex fugit hic, consequatur ut, labore beatae. Officiis magni, corrupti autmpora distinctio sit cum fuga consequuntur ex mpora distinctio sit </div>
+                {/* < className="w-full  text-sm rounded-xl py-2 bgimg  text-center text-white"><bu>View more images</bu */}
+                <div><button className='mt-1 py-1 w-[90%] bgimg bg-no-repeat bg-cover rounded-md hover:bg-red-600 hover:text-white'>Donate Now</button></div>
+            </div>
+
+            <div className="flex-col h-[400px] border-2 w-[300px] shadow-xl rounded-lg mb-5 hover:scale-95 transition duration-100 ease-in xsm:h-auto">
+                <div className=""><img className="rounded-t w-[350px] m-auto h-48 xsm:h-44" src="img1.png" alt="" /></div>
+                <div className="flex justify-between">
+                    <div className="bgimg bg-no-repeat text-black px-1 mt-1 text-xs py-1 rounded-tr rounded-br">Event Name</div>
+                    <div className="bgimg  mt-1 text-xs px-1 py-1 rounded-tl rounded-bl">Date</div>
+                </div>
+                <div className="px-1 mt-1 font-bold text-center xsm:text-sm">Title</div>
+                <div className="px-2 xsm:text-xs mt-1 text-justify text-sm ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora distinctio sit cum fuga consequuntur ex fugit hic, consequatur ut, labore beatae. Officiis magni, corrupti autmpora distinctio sit cum fuga consequuntur ex mpora distinctio sit </div>
+                {/* < className="w-full  text-sm rounded-xl py-2 bgimg  text-center text-white"><bu>View more images</bu */}
+                <div><button className='mt-1 py-1 w-[90%] bgimg bg-no-repeat bg-cover rounded-md hover:bg-red-600 hover:text-white'>Donate Now</button></div>
+            </div>
+
+            <div className="flex-col h-[400px] border-2 w-[300px] shadow-xl rounded-lg mb-5 hover:scale-95 transition duration-100 ease-in xsm:h-auto">
+                <div className=""><img className="rounded-t w-[350px] m-auto h-48 xsm:h-44" src="img1.png" alt="" /></div>
+                <div className="flex justify-between">
+                    <div className="bgimg bg-no-repeat text-black px-1 mt-1 text-xs py-1 rounded-tr rounded-br">Event Name</div>
+                    <div className="bgimg  mt-1 text-xs px-1 py-1 rounded-tl rounded-bl">Date</div>
+                </div>
+                <div className="px-1 mt-1 font-bold text-center xsm:text-sm">Title</div>
+                <div className="px-2 xsm:text-xs mt-1 text-justify text-sm ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora distinctio sit cum fuga consequuntur ex fugit hic, consequatur ut, labore beatae. Officiis magni, corrupti autmpora distinctio sit cum fuga consequuntur ex mpora distinctio sit </div>
+                {/* < className="w-full  text-sm rounded-xl py-2 bgimg  text-center text-white"><bu>View more images</bu */}
+                <div><button className='mt-1 py-1 w-[90%] bgimg bg-no-repeat bg-cover rounded-md hover:bg-red-600 hover:text-white'>Donate Now</button></div>
+            </div>
+            </div>
+            <div className='flex justify-evenly'>
+            <div className="flex-col h-[400px] border-2 w-[390px] shadow-xl rounded-lg mb-5 hover:scale-95 transition duration-100 ease-in xsm:h-auto">
+                <div className=""><img className="rounded-t w-[350px] m-auto h-48 xsm:h-44" src="img1.png" alt="" /></div>
+                <div className="flex justify-between">
+                    <div className="bgimg bg-no-repeat text-black px-1 mt-1 text-xs py-1 rounded-tr rounded-br">Event Name</div>
+                    <div className="bgimg  mt-1 text-xs px-1 py-1 rounded-tl rounded-bl">Date</div>
+                </div>
+                <div className="px-1 mt-1 font-bold text-center xsm:text-sm">Title</div>
+                <div className="px-2 xsm:text-xs mt-1 text-justify text-sm ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora distinctio sit cum fuga consequuntur ex fugit hic, consequatur ut, labore beatae. Officiis magni, corrupti autmpora distinctio sit cum fuga consequuntur ex mpora distinctio sit </div>
+                {/* < className="w-full  text-sm rounded-xl py-2 bgimg  text-center text-white"><bu>View more images</bu */}
+                <div><button className='mt-1 py-1 w-[90%] bgimg bg-no-repeat bg-cover rounded-md hover:bg-red-600 hover:text-white'>Donate Now</button></div>
+            </div>
+
+            
+
+            
+            </div>
+        </div>
+    
+    
+    </div>
+    </div>
+{/* </div> */}
+    
+    
+    </>)
+}
