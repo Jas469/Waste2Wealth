3fd79a0c60ef683922f75a048e9618c1cf5dea76
Harivansh Bhardwaj <harivanshbhardwaj57@gmail.com>
2023-10-28 17:44:02 +0530

    push

---
+++ pages/AboutUs.js
@@ -0,0 +1,146 @@
+
+// export default function AboutUs() {
+//   return (<>
+//     <div className="ct-pageWrapper" id="ct-js-wrapper">
+//       <section className="company-heading intro-type" id="parallax-one">
+//         <div className="container">
+//           <div className="row product-title-info">
+//             <div className="col-md-12">
+//               <h1>About Us</h1>
+//             </div>
+//           </div>
+//         </div>
+//         <div className="parallax" id="parallax-cta" style={{ backgroundImage: 'url(images/hero.jpg)' }}>
+//           &nbsp;
+//         </div>
+//       </section>
+//       <section className="story-section company-sections ct-u-paddingBoth100 paddingBothHalf noTopMobilePadding" id="section">
+//         <div className="container text-center">
+//           <h2>WHAT DRIVES US</h2>
+//           <h3></h3>
+//           <div className="col-md-8 col-md-offset-2">
+//             <div className="red-border">
+//               &nbsp;
+//             </div>
+//             <p className="ct-u-size22 ct-u-fontWeight300 marginTop40">
+//               Welcome to Waste2Wealth, your gateway to transforming waste into a valuable resource. At Waste2Wealth, we're on a mission to make a meaningful difference in the world by revolutionizing the way we handle waste materials, with a primary focus on waste plastics.
+//             </p>
+//           </div>
+//         </div>
+//       </section>
+//       <section className="culture-section company-sections ct-u-paddingBoth100 paddingBothHalf noTopMobilePadding">
+//         <div className="container">
+//           <div className="col-md-8 col-md-offset-2">
+//             <h2>Our Mission</h2>
+//             <h3></h3>
+//             <p className="ct-u-size22 ct-u-fontWeight300 ct-u-marginBottom60">
+//               Our mission is straightforward but powerful: to facilitate the efficient and sustainable management of waste plastics by connecting individuals, businesses, and communities with local recycling solutions. By doing so, we aim to reduce the environmental impact of plastic waste and contribute to a cleaner, more prosperous planet.
+//             </p>
+//             <br />
+//             <h2>Why Waste2Wealth?</h2>
+//             <br />
+//             <br />
+//             <br />
+//           </div>
+//           <div className="row ct-u-paddingBoth20">
+//             <div className="col-xs-6 col-md-4">
+//               <div className="company-icons-white">
+//                 <i className="fa fa-medkit" aria-hidden="true"></i>
+//                 <p>Environmental Leadership</p>
+//                 <p className="company-icons-subtext hidden-xs"></p>
+//               </div>
+//             </div>
+//             <div className="col-xs-6 col-md-4">
+//               <div className="company-icons-white">
+//                 <i className="fa fa-money" aria-hidden="true"></i>
+//                 <p>Financial Rewards</p>
+//                 <p className="company-icons-subtext hidden-xs"></p>
+//               </div>
+//             </div>
+//             <div className="col-xs-6 col-md-4">
+//               <div className="company-icons-white">
+//                 <i className="fa fa-clock-o" aria-hidden="true"></i>
+//                 <p>Local Sustainability</p>
+//                 <p className="company-icons-subtext hidden-xs"></p>
+//               </div>
+//             </div>
+//           </div>
+//           <div className="row ct-u-paddingBoth20">
+//             <div className="col-xs-6 col-md-4">
+//               <div className="company-icons-white">
+//                 <i className="fa fa-coffee" aria-hidden="true"></i>
+//                 <p>User-Friendly Experience</p>
+//                 <p className="company-icons-subtext hidden-xs"></p>
+//               </div>
+//             </div>
+//             <div className="col-xs-6 col-md-4">
+//               <div className="company-icons-white">
+//                 <i className="fa fa-gamepad" aria-hidden="true"></i>
+//                 <p>NEC MATTIS</p>
+//                 <p className="company-icons-subtext hidden-xs"></p>
+//               </div>
+//             </div>
+//             <div className="col-xs-6 col-md-4">
+//               <div className="company-icons-white">
+//                 <i className="fa fa-cutlery" aria-hidden="true"></i>
+//                 <p>CRAS MOLLIS</p>
+//                 <p className="company-icons-subtext hidden-xs"></p>
+//               </div>
+//             </div>
+//           </div>
+//           <a className="ct-u-marginTop60 btn btn-solodev-red-reversed btn-fullWidth-sm ct-u-size19" href="/">Ready to Learn More?</a>
+//         </div>
+//       </section>
+//       <section className="customers-section company-sections ct-u-paddingBoth100 paddingBothHalf noTopMobilePadding">
+//         <div className="container">
+//           <div className="col-md-8 col-md-offset-2">
+//             <h2>CUSTOMERS</h2>
+//             <h3>Trusted by some of the world&rsquo;s leading brands.</h3>
+//             <p className="ct-u-size22 ct-u-fontWeight300 ct-u-marginBottom60 marginTop40"></p>
+//           </div>
+//           <div className="clearfix">
+//             &nbsp;
+//           </div>
+//         </div>
+//         <div className="container ct-u-paddingTop40 ct-u-paddingBottom100">
+//           <div className="row">
+//             <div className="col-md-12 ct-content">
+//               <section className="clients-home">
+//                 <div className="container">
+//                   <div className="clients-logos text-center">
+//                     {/* starting row div */}
+//                     <div className="row">
+//                       <div className="col-md-4 client-logos-repeater">
+//                         <a href="/path/to/detail/zeina.html" className="Zeina"><img alt="images/logo-zeina.png" src="images/logo-zeina.png" /></a>
+//                         <div className="logo-title">
+//                           <div className="displayTable">
+//                             <div className="displayTableCell">Zeina - 0</div>
+//                           </div>
+//                         </div>
+//                       </div>
+//                       <div className="col-md-4 client-logos-repeater">
+//                         <a href="/path/to/detail/logic.html" className="Logic"><img alt="images/logic.png" src="images/logic.png" /></a>
+//                         <div className="logo-title">
+//                           <div className="displayTable">
+//                             <div className="displayTableCell">Logic</div>
+//                           </div>
+//                         </div>
+//                       </div>
+//                       <div className="col-md-4 client-logos-repeater">
+//                         <a href="/path/to/detail/smart.html" className="Smart"><img alt="images/client3.png" src="images/client3.png" /></a>
+//                         <div className="logo-title">
+//                           <div className="displayTable">
+//                             <div className="displayTableCell">Smart</div>
+//                           </div>
+//                         </div>
+//                       </div>
+//                     </div>
+//                     {/* closing row div */}
+//                   </div>
+//                 </div>
+//               </section>
+//             </div>
+           
+
+//             </>)
+//             }
